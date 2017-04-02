489bffbd647739f303248b4843e1de27a5e1e65e
waylybaye <havelove@gmail.com>
2017-04-02 17:56:52 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## HyperApp I18N
+
+### How to contribute
+
+1. Copy `zh.js` to another locale like `jp.js`
+2. Edit the copied file and change the text
+3. create PR
+
+
+### Thanks for your help!
---
+++ zh.js
@@ -0,0 +1,99 @@
+{
+    Confirm: '请确认',
+    Ok: '好',
+    Cancel: '取消',
+    Error: '错误',
+    Save: '保存',
+
+    support: {
+      send_email: '发送反馈邮件',
+      rate_us: '去 App Store 评分',
+      share: '把 HyperApp 分享给朋友',
+    },
+
+    apps: {
+      nothing_placeholder: '还没有部署应用呢，去商店看看吧。',
+      action: {
+        edit: '编辑',
+        config: '配置',
+        history: '历史',
+        start: '启动',
+        stop: '关闭',
+        install: '安装',
+        logs: '查看日志',
+        update_config: '更新配置',
+        uninstall: '卸载',
+        uninstall_config: '您确定要卸载此应用吗？',
+        restart: '重启',
+        restart_confirm: '您确定要重启此应用吗？',
+        delete: '删除',
+        delete_confirm: '您确定要删除此应用吗？',
+      }
+    },
+
+    config_app: {
+      tip: "保存后需要手动更新配置",
+      next: "下一步",
+      skip: "跳过",
+    },
+
+    servers: {
+      nothing_placeholder: '点击添加按钮开始 HyperApp 之旅吧。',
+
+      status: {
+        undefined: '',
+        connecting: '正在连接',
+        authenticating: '正在认证',
+        running: '运行中',
+        stopped: '已关机',
+      },
+      action: {
+        logs: '日志',
+        delete: '删除',
+        delete_confirm: '您确定要删除该服务器吗？',
+        shutdown: '关机',
+        reboot: '重启',
+      }
+    },
+
+    add_server: {
+      form: {
+        add_form_title: '添加服务器',
+        edit_form_title: '编辑服务器',
+        add_button_title: '添加服务器',
+        name: '名称',
+        name_placeholder: '自定义名称',
+        host: '主机',
+        host_placeholder: '域名或者IP',
+        port: '端口',
+        user: '用户名',
+        user_placeholder: '需要 sudo 权限',
+        password: '密码',
+        password_placeholder: '登录密码',
+        passphrase: '私钥密码',
+        passphrase_placeholder: '',
+        paste_private_key_from_clipboard: '从剪贴板中复制私钥',
+      }
+    },
+
+    nav: {
+      store: '商店',
+      apps: '应用',
+      servers: '服务器',
+      help: '帮助',
+    },
+
+    ssh: {
+      undefined: '',
+      connecting: '连接中',
+      authenticating: '认证中',
+    },
+
+    add_app: {
+      form: {
+        name: '名字'
+      },
+      add_title: '创建应用',
+      no_server_tip: '您目前没有创建服务器，创建应用后需要部署到服务器才能使用。',
+    }
+  }
